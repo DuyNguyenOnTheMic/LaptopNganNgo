9fe2814d1bacda64d17e8ba13108b30e4c22fa59
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2021-05-01 21:23:09 +0700

    Doi nhieu thu

--- Test/Controllers/Website_BanHang/DangNhapController.cs
+++ Test/Controllers/Website_BanHang/DangNhapController.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Test.Models;
+using System.Web.Security;
 
-namespace Test.Controllers
+namespace Test.Controllers.Website_BanHang
 {
     public class DangNhapController : Controller
     {
         // GET: DangNhap
         public ActionResult Index()
         {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(Models.Membership model)
+        {
+            using (var context = new CT25Team24Entities())
+            {
+                bool isValid = context.KhachHangs.Any(x => x.TenDangNhap == model.TenDangNhap
+                && x.MatKhau == model.MatKhau);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.TenDangNhap, false);
+                    return RedirectToAction("Index", "TrangChu");
+                }
+            }
+            ModelState.AddModelError("", "Invalid username and password!!");
             return View();
         }
     }
---
+++ Test/Models/Membership.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class Membership
+    {
+        public int MaKH { get; set; }
+        public string HoTen { get; set; }
+        public int GioiTinh { get; set; }
+        public string DienThoai { get; set; }
+        public string DiaChi { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string TenDangNhap { get; set; }
+        public string Email { get; set; }
+        public string MatKhau { get; set; }
+        public string VaiTro { get; set; }
+
+    }
+}
