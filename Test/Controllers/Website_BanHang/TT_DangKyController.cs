35b65470767a194172746bca060efd9925491a0e
DuyNTOnTheMic <duy.197pm21905@vanlanguni.vn>
2021-05-01 11:49:06 +0700

    Do an ne

---
+++ Test/Controllers/Website_BanHang/TT_DangKyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Controllers
+{
+    public class TT_DangKyController : Controller
+    {
+        // GET: TT_DangKy
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Test/Controllers/Website_BanHang/TT_DangNhapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Controllers
+{
+    public class TT_DangNhapController : Controller
+    {
+        // GET: TT_DangNhap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
