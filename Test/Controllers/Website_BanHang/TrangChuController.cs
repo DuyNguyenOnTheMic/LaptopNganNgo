d4758830b2adcd0aec1fc72eab5e1e047ac91fff
197PM33932 - TRẦN QUỐC NAM - K25T-PM1 <nam.197pm33932@vanlanguni.vn>
2021-05-09 13:43:53 +0700

    Cập nhật tính năng load sản phẩm

--- Test/Controllers/Website_BanHang/TrangChuController.cs
+++ Test/Controllers/Website_BanHang/TrangChuController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Test.Models;
 
 namespace Test.Controllers
 {
     public class TrangChuController : Controller
     {
+        CT25Team24Entities db = new CT25Team24Entities();
+        
         // GET: TrangChu
-        public ActionResult Index()
+        public ActionResult Index2()
         {
-            return View();
+            var model = db.SanPhams.ToList();
+            return View(model);
         }
     }
 }
