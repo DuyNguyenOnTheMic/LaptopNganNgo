833887db29c89e883abea7e05dc30d2aa07c87a2
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2021-05-10 16:49:09 +0700

    Thêm unit Test

--- UnitTest_LaptopNganNgo/UnitTest1.cs
+++ UnitTest_LaptopNganNgo/UnitTest1.cs
@@ -6,9 +6,18 @@ namespace UnitTest_LaptopNganNgo
     [TestClass]
     public class UnitTest1
     {
+        private double CongHaiSo(double so1, double so2)
+        {
+            return so1 + so2;
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            double soThuNhat = 10;
+            double soThuHai = 20;
+            double kq = CongHaiSo(soThuNhat, soThuHai);
+            Assert.AreEqual(30, kq);
         }
     }
 }
