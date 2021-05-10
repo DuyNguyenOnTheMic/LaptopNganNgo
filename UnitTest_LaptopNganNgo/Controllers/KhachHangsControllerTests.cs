5db86779c7c912b9d86948e65451c2dfc97487cd
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2021-05-10 17:46:15 +0700

    Update test Index Controller Khách hàng

--- UnitTest_LaptopNganNgo/Controllers/KhachHangsControllerTests.cs
+++ UnitTest_LaptopNganNgo/Controllers/KhachHangsControllerTests.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
+using Test.Models;
+using System.Net;
+
 
 namespace Test.Controllers.Website_QuanTri.Tests
 {
@@ -14,7 +18,18 @@ namespace Test.Controllers.Website_QuanTri.Tests
         [TestMethod()]
         public void IndexTest()
         {
-            Assert.Fail();
+            //Arrange
+            var controller = new KhachHangsController();
+            var db = new CT25Team24Entities();
+
+            //Act
+            var result = controller.Index() as ViewResult;
+            var model = result.Model as List<KhachHang>;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(model);
+            Assert.AreEqual(db.KhachHangs.Count(), model.Count);
         }
     }
 }
